6e7f13aba767b27edb5bef3f81c484e3a8fb04a5
Ryan Kenney <rklazerbeams@gmail.com>
2015-08-06 04:08:50 -0700

    change hide class to hidden

--- js/bootstrap-tabdrop.js
+++ js/bootstrap-tabdrop.js
@@ -56,9 +56,9 @@
 		this.options = options;
 
 		if (options.align === "left")
-			this.dropdown = $('<li class="dropdown hide pull-left tabdrop"><a class="dropdown-toggle" data-toggle="dropdown" href="#"><span class="display-tab"></span><b class="caret"></b></a><ul class="dropdown-menu"></ul></li>');
+			this.dropdown = $('<li class="dropdown hidden pull-left tabdrop"><a class="dropdown-toggle" data-toggle="dropdown" href="#"><span class="display-tab"></span><b class="caret"></b></a><ul class="dropdown-menu"></ul></li>');
 		else
-			this.dropdown = $('<li class="dropdown hide pull-right tabdrop"><a class="dropdown-toggle" data-toggle="dropdown" href="#"><span class="display-tab"></span><b class="caret"></b></a><ul class="dropdown-menu"></ul></li>');
+			this.dropdown = $('<li class="dropdown hidden pull-right tabdrop"><a class="dropdown-toggle" data-toggle="dropdown" href="#"><span class="display-tab"></span><b class="caret"></b></a><ul class="dropdown-menu"></ul></li>');
 
 		this.dropdown.prependTo(this.element);
 		if (this.element.parent().is('.tabs-below')) {
@@ -130,7 +130,7 @@
 
 				if (collection.length > 0) {
 					if (!recursion) {
-						self.dropdown.removeClass('hide');
+						self.dropdown.removeClass('hidden');
 						self.dropdown.find('ul').empty();
 					}
 					self.dropdown.find('ul').prepend(collection);
@@ -147,7 +147,7 @@
 					checkOffsetAndPush(true);
 				} else {
 					if (!recursion) {
-						self.dropdown.addClass('hide');
+						self.dropdown.addClass('hidden');
 					}
 				}
 			}
